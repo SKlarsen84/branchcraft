2da76cee40ffab157ed03ba36319c3a2536de40e
Stefan Larsen <stefan.larsen@northstake.dk>
2023-04-11 10:14:19 +0200

    readd logging

--- src/cli.ts
+++ src/cli.ts
@@ -201,6 +201,10 @@ async function chatGptRequest(
 
   const text = ((response.data.choices[0].message as any).content as string).trim()
 
+  // Save the entire chat history to a file in the user's home directory with the date and time as the file name
+  const historyPath = `log.branchcraft.latest.txt`
+  await fs.writeFile(historyPath, JSON.stringify(history, null, 2), 'utf-8')
+
   // Replace double backslashes with single backslashes and remove extra quotes
   return text.replace(/\\/g, '\\').replace(/^"|"$/g, '')
 }
